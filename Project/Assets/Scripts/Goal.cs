4a884028ad7ab82236fc35d35a30338c04e2d185
KeksDerKruemelige <kevinmi7@gmx.de>
2018-04-28 16:28:57 +0200

    Revert "Merge branch 'master' of https://github.com/Till-Later/toolate"
    This reverts commit b5f53c7c73866c8fe0b2030591395ed42c3933b4, reversing
    changes made to 14161d323e816af51963c2d38e0263c727377fda.

--- Project/Assets/Scripts/Goal.cs
+++ Project/Assets/Scripts/Goal.cs
@@ -3,27 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Goal : MonoBehaviour {
-    [SerializeField]
-    SpriteRenderer sprite;
 
-    void Start () {
-
+	// Use this for initialization
+	void Start () {
+		
 	}
 	
+	// Update is called once per frame
 	void Update () {
-
+		
 	}
-
-    void OnTriggerEnter2D(Collider2D collider) {
-        Color spriteColor = sprite.color;
-        spriteColor.a = 0.5f;
-        sprite.color = spriteColor;
-        Debug.Log("Great!");
-    }
-
-    void OnTriggerExit2D(Collider2D collider) {
-        Color spriteColor = sprite.color;
-        spriteColor.a = 1f;
-        sprite.color = spriteColor;
-    }
 }
