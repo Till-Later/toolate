14cbd95074a72ece05a7024164d41e98bc32ac5f
Till Lehmann <till.lehmann@student.hpi.uni-potsdam.de>
2018-04-28 16:47:32 +0200

    Added moving obstacle

--- Project/Assets/Scripts/MovingObstacle.cs
+++ Project/Assets/Scripts/MovingObstacle.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class MovingObstacle : MonoBehaviour {
 
+	public float maxPositionShift = 0;
+	public float speed = 1;
+	float positionShift = 0;
+	bool positive = true;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +16,17 @@ public class MovingObstacle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (positive) {
+			positionShift += speed;
+			transform.position += new Vector3 (speed,0F,0F) * Time.deltaTime;	
+			if (positionShift > maxPositionShift)
+				positive = !positive;
+		} else {
+			positionShift -= speed;
+			transform.position -= new Vector3 (speed,0F,0F) * Time.deltaTime;	
+			if (positionShift < -maxPositionShift)
+				positive = !positive;
+		}
+
 	}
 }
