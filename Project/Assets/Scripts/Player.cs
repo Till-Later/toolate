14161d323e816af51963c2d38e0263c727377fda
Tobias Kohl <tobiko10@aol.com>
2018-04-28 15:46:57 +0200

    Player.Die()

--- Project/Assets/Scripts/Player.cs
+++ Project/Assets/Scripts/Player.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public LoopArea loopArea;
+    public float minDistanceToObstacleForward;
+
+    private Vector2 collissionCheckVectorA, collissionCheckVectorB;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        collissionCheckVectorA = new Vector2(-gameObject.transform.localScale.x / 2, minDistanceToObstacleForward + gameObject.transform.localScale.y / 2);
+        collissionCheckVectorB = new Vector2(gameObject.transform.localScale.x / 2, -gameObject.transform.localScale.y / 2);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void Die()
+    {
+        float respawnYPos;
+        Collider2D[] colliders;
+        Vector2 proposedPosition;
+        do
+        {
+            respawnYPos = Random.Range(0f, loopArea.gameObject.transform.position.y);
+            proposedPosition = new Vector2(0, respawnYPos);
+            colliders = Physics2D.OverlapAreaAll(proposedPosition + collissionCheckVectorA, proposedPosition + collissionCheckVectorB);
+        } while (colliders.Length > 0);
+
+        gameObject.transform.localPosition = proposedPosition;
+    }
 }
