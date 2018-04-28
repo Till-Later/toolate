5321cf098b18716d6215d30672cbbfb2ecfd8038
Jannis Baudisch <jannis.baudisch@nocapes.de>
2018-04-28 18:36:51 +0200

    LevelManager

---
+++ Project/Assets/LevelPopupStartGame.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPopupStartGame : StateMachineBehaviour {
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        Debug.Log("Animationb is done");
+        animator.GetComponent<LevelPopup>().playerController.enabled = true;
+    }
+}
---
+++ Project/Assets/Scripts/LevelPopup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPopup : MonoBehaviour {
+    public PlayerController playerController;
+    public Animator animator;
+
+    public void EndLevel() {
+        animator.SetTrigger("End");
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) { EndLevel(); }
+    }
+}
